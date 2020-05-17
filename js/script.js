c5d288cfa1fb9799f24a72aca19d1c71d6765a0d
Vipul <vipul.bisht96@gmail.com>
2020-05-18 00:16:30 +0530

    Gulp Part 2

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-$("#carouselButton").click(function(){
-    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
-        $("#mycarousel").carousel('pause');
-        $("#carouselButton").children("span").removeClass('fa-pause');
-        $("#carouselButton").children("span").addClass('fa-play');
-    }
-    else if ($("#carouselButton").children("span").hasClass('fa-play')){
-        $("#mycarousel").carousel('cycle');
-        $("#carouselButton").children("span").removeClass('fa-play');
-        $("#carouselButton").children("span").addClass('fa-pause');                    
-    }
-});
-$("#loginButton").click(function(){
-$("#loginModal").modal('show');
-});
-
-$("#reserveButton").click(function(){
-$("#reserveModal").modal('show');
+$(document).ready(function(){
+	$("#mycarousel").carousel( { interval: 2000 } );
+	$("#carousel-button").click(function(){
+		if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+			$("#mycarousel").carousel('pause');
+			$("#carousel-button").children("span").removeClass('fa-pause');
+			$("#carousel-button").children("span").addClass('fa-play');
+		}
+		else if ($("#carousel-button").children("span").hasClass('fa-play')){
+			$("#mycarousel").carousel('cycle');
+			$("#carousel-button").children("span").removeClass('fa-play');
+			$("#carousel-button").children("span").addClass('fa-pause');                    
+		}
+	});
+	$('#reserveButton').click(function(){
+		$('#reserveModal').modal('show');
+	});
+	$('#loginButton').click(function(){
+		$('#loginModal').modal('show');
+	});
 });
